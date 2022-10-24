a0bfc1c39a7ac647fb9c130fd27b5f2babbe4465
Atcheri Atch <atcheri@gmail.com>
2022-10-24 21:18:50 +0200

    feat(translations): add translation POST handler (2)

---
+++ internal/core/domain/language_translation.go
@@ -0,0 +1,22 @@
+package domain
+
+// LanguageTranslation is
+type LanguageTranslation struct {
+	code string
+	text string
+}
+
+func NewLanguageTranslation(code, text string) (LanguageTranslation, error) {
+	// validate code and text here
+	return LanguageTranslation{
+		code: code,
+		text: text,
+	}, nil
+}
+
+func (t LanguageTranslation) GetCode() string {
+	return t.code
+}
+func (t LanguageTranslation) GetText() string {
+	return t.text
+}
